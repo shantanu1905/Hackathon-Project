b375c60f7a862dd6b7d5e779004537802d17c2a0
shantanu1905 <nimkarshantanu07@gmail.com>
2022-06-30 00:29:00 +0530

    serializers.py updated

--- api_floodmanagement/serializers.py
+++ api_floodmanagement/serializers.py
@@ -6,7 +6,8 @@ from api_floodmanagement.models import *
 
 class HelpSerializer(serializers.ModelSerializer):
     owner = serializers.CharField(read_only=True, default=serializers.CurrentUserDefault())
+    RequestStatus = models.CharField()
     class Meta:
         model = UserHelpRequest
-        fields = ['id', 'latitude', 'longitude','created', 'TypeOfEmergency' ,'owner']
+        fields = ['id', 'latitude', 'longitude','created', 'TypeOfEmergency' ,'owner' , 'RequestStatus']
         
