ee7afd2b73804e90f01662be7c3dc9a0371e2456
shantanu1905 <nimkarshantanu07@gmail.com>
2022-12-28 14:44:07 +0530

    f

--- configurations/settings.py
+++ configurations/settings.py
@@ -146,7 +146,7 @@ USE_L10N = True
 USE_TZ = True
  
 
-# Static files (CSS, JavaScript, Images)
+# Staticc files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_URL = '/static/'
 
