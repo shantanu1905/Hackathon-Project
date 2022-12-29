191e7e1e9d9aef2c9577bb7b3857e806a9ba81b8
shantanu1905 <nimkarshantanu07@gmail.com>
2022-12-29 23:14:43 +0530

    ud4

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 echo " BUILD START"
-python3.7 -m pip install -r requirements.txt
-
+python3.7.12 -m pip install -r requirements.txt
+python3.7.12 manage.py collectstatic --noinput --clear
 echo " BUILD STOP"
